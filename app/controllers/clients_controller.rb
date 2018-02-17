71cfbafd7cb1f40db8c35c23b179bc28f12a009f
morl0048 <morneaulouka@hotmail.com>
2018-02-17 14:58:40 -0500

    debut dajout dun formulaire simple + correction du controller (manque end)

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -4,28 +4,41 @@ class ClientsController < ApplicationController
   # GET /clients.xml
     def index
       @clients = Client.all
-
       respond_to do |format|
         format.html #index.html.erb
         format.xml { render :xml => @clients }
       end
     end
 
+# GET /clients/client1
+# GET /clients/client1.xml
+  def show
+    @client = Client.find(params[:id])
+    respond_to do |format|
+      format.html #show.html.erb
+      format.xml  { render :xml => @client }
+  end
+end
+
 # GET /clients/new
 # GET /clients/new.xml
   def new
     @client = Client.new
-
     respond_to do |format|
       format.html #new.html.erb
       format.xml { render :xml => @client }
+    end
+  end
+
+# GET /clients/client1/edit
+  def edit
+    @client = Client.find(params[:id])
   end
 
 # POST /clients
 # POST /clients.xml
   def create
     @client = Client.new(client_params)
-
     respond_to do |format|
       if @client.save
         format.html { redirect_to(@client, :notice => 'Client was successfully created.') }
@@ -41,7 +54,6 @@ class ClientsController < ApplicationController
 # PUT /clients/client1.xml
   def update
     @client = Client.find(params[:id])
-
     respond_to do |format|
       if @client.update_attributes(client_params)
         format.html { redirect_to(@client, :notice => 'Client was successfully updated.') }
@@ -51,11 +63,6 @@ class ClientsController < ApplicationController
         format.xml  { render :xml => @client.errors, :status => :unprocessable_entity }
       end
     end
-  end
-
-# GET /clients/client1/edit
-  def edit
-    @client = Client.find(params[:id])
   end
 
 # DELETE /clients/client1
@@ -70,24 +77,13 @@ class ClientsController < ApplicationController
     end
   end
 
-# GET /clients/client1
-# GET /clients/client1.xml
-  def show
-    @client = Client.find(params[:id])
-
-    respond_to do |format|
-      format.html #show.html.erb
-      format.xml  { render :xml => @client }
-  end
-end
-
-private
+###########################################################################
+  private
 
   def client_params
     params.require(:client).permit(
       :prenom, :nom, :dateNaissance, :nas, :nombreEnfants, :compteTaxesProprietaire,
       addresses_attributes: [:id, :numeroCivique, :rue, :codePostal, :ville, :province]
     )
-  end
   end
 end
