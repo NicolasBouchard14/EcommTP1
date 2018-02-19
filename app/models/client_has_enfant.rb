8f041f3bda51adbfe1e1fc473d6dfbc87236050c
morl0048 <morneaulouka@hotmail.com>
2018-02-19 14:13:40 -0500

    je veux avoir les radiobuttons comme du monde

--- app/models/client_has_enfant.rb
+++ app/models/client_has_enfant.rb
@@ -1,4 +1,5 @@
 class ClientHasEnfant < ApplicationRecord
   belongs_to :enfant
   belongs_to :client
+  accepts_nested_attributes_for :enfant, :allow_destroy => true
 end
--- app/models/enfant.rb
+++ app/models/enfant.rb
@@ -1,4 +1,5 @@
 class Enfant < ApplicationRecord
-  has_many :client_has_enfants
-  has_many :clients, :through => :client_has_enfants
+  has_many :client_has_enfants,
+  has_many :clients, through: :client_has_enfants
 end
+#inverse_of: :enfant
