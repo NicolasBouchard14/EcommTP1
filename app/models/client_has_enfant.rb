7d3673b1fe88fd2e5bb2e9be8e51536bde5b17b0
Nicolas Bouchard <bouchardn.tech@gmail.com>
2018-02-19 14:17:28 -0500

    Modification modele clientHasEnfant

--- app/models/client_has_enfant.rb
+++ app/models/client_has_enfant.rb
@@ -2,4 +2,5 @@ class ClientHasEnfant < ApplicationRecord
   belongs_to :enfant
   belongs_to :client
   enum lien: [:'Père/fils', :'Père/fille', :'Mère/fils', :'Mère/fille:', :Autre]
+  accepts_nested_attributes_for :enfant, :allow-destroy => true
 end
