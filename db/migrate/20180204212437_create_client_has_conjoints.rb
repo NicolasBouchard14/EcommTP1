9e2ebae00362b3a006b29bcf4d6e2866ed581e44
Nicolas Bouchard <bouchardn.tech@gmail.com>
2018-02-18 21:17:16 -0500

    Modifications des pluriels au niveau des clés étrangères

--- db/migrate/20180204212437_create_client_has_conjoints.rb
+++ db/migrate/20180204212437_create_client_has_conjoints.rb
@@ -1,7 +1,7 @@
 class CreateClientHasConjoints < ActiveRecord::Migration[5.1]
   def change
     create_table :clients_has_conjoint do |t|
-      t.references :clients, foreign_key: true
+      t.references :client, foreign_key: true
       t.datetime :dateDebut
       t.datetime :dateFin
     end
--- db/migrate/20180204220244_create_client_etat_civils.rb
+++ db/migrate/20180204220244_create_client_etat_civils.rb
@@ -1,8 +1,8 @@
 class CreateClientEtatCivils < ActiveRecord::Migration[5.1]
   def change
     create_table :client_etat_civils do |t|
-      t.references :etat_civils, foreign_key: true
-      t.references :clients, foreign_key: true
+      t.references :etat_civil, foreign_key: true
+      t.references :client, foreign_key: true
       t.datetime :dateDebut
       t.datetime :dateFin
     end
