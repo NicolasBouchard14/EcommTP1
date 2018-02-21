9bcc1c52016cea91bf655c401dcc02fc4c900159
Nicolas Bouchard <bouchardn.tech@gmail.com>
2018-02-20 21:05:05 -0500

    tests fonctionnels minimals

--- test/features/can_access_root_test.rb
+++ test/features/can_access_root_test.rb
@@ -3,6 +3,6 @@ require "test_helper"
 feature "CanAccessRoot" do
   scenario "test_de_base" do
      visit root_path
-     page.must_have_content "Hello World"
+     page.must_have_content "Liste des clients"
      end
 end
