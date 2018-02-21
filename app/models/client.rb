aa54ba64d2f8a1f6fb1b5f28461e28595ac7d5c0
morl0048 <morneaulouka@hotmail.com>
2018-02-21 09:30:28 -0500

    conjoint fonctionne

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,7 +2,8 @@ class Client < ApplicationRecord
   belongs_to :addresse, :class_name => 'Addresse', :dependent => :destroy
   has_many :client_has_enfants, :dependent => :destroy
   has_many :enfants, :through => :client_has_enfants, :dependent => :destroy
-  has_many :client_has_conjoint, :dependent => :destroy
+  has_many :client_has_conjoints, :dependent => :destroy
+  has_many :conjoints, :through => :client_has_conjoints, :dependent => :destroy
   has_many :etudes, :dependent => :destroy
   has_many :institutions, :through => :etudes, :dependent => :destroy
   has_many :client_etat_civils, :dependent => :destroy
@@ -15,7 +16,8 @@ class Client < ApplicationRecord
   accepts_nested_attributes_for :etat_civils, :allow_destroy => true
   accepts_nested_attributes_for :client_has_employeurs, :allow_destroy => true
   accepts_nested_attributes_for :employeurs, :allow_destroy => true
-  accepts_nested_attributes_for :client_has_conjoint, :allow_destroy => true
+  accepts_nested_attributes_for :client_has_conjoints, :allow_destroy => true
+  accepts_nested_attributes_for :conjoints, :allow_destroy => true
   accepts_nested_attributes_for :addresse, :allow_destroy => true
   accepts_nested_attributes_for :etudes, :allow_destroy => true
   accepts_nested_attributes_for :institutions, :allow_destroy => true
